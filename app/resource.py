7010a6f84671f45b54f1bb766f5c065a631d08f7
Joaquin Lepez <joacolepez@gmail.com>
2024-10-09 09:22:37 -0300

    Add Validation in resource

--- app/resource.py
+++ app/resource.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from marshmallow import ValidationError
 from .services import ProductoService, ResponseBuilder
 from .mapping import ProductoSchema, ResponseSchema
 
@@ -19,10 +20,14 @@ def index():
 @catalogo.route('/productos/add', methods=['POST'])
 def add():
     response_builder = ResponseBuilder()
-    producto = producto_schema.load(request.json)
-    data = producto_schema.dump(producto_service.add(producto))
-    response_builder.add_message("Producto added").add_status_code(201).add_data(data)
-    return response_schema.dump(response_builder.build()), 201
+    try:
+        producto = producto_schema.load(request.json)
+        data = producto_schema.dump(producto_service.add(producto))
+        response_builder.add_message("Producto added").add_status_code(201).add_data(data)
+        return response_schema.dump(response_builder.build()), 201
+    except ValidationError as err:
+        response_builder.add_message("Validation error").add_status_code(422).add_data(err.messages)
+        return response_schema.dump(response_builder.build()), 422
 
 @catalogo.route('/productos/<int:id>', methods=['GET'])
 def find_activo(id):
