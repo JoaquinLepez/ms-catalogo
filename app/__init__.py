cc8588cb85ca5e6b5ca2a27caa65eb91610e72b0
Joaquin Lepez <joacolepez@gmail.com>
2024-11-30 17:57:51 -0300

    Add migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
+from flask_migrate import Migrate
+from sqlalchemy import MetaData
 import os
-from pdchaos.middleware.contrib.flask.flask_middleware import FlaskMiddleware
+# from pdchaos.middleware.contrib.flask.flask_middleware import FlaskMiddleware
 from app.config import config, cache_config
 
-db = SQLAlchemy()
-middleware = FlaskMiddleware()
+db = SQLAlchemy(metadata= MetaData(schema= 'catalogo'))
+migrate = Migrate()
+# middleware = FlaskMiddleware()
 cache = Cache()
 
 def create_app():
@@ -17,11 +20,12 @@ def create_app():
     configuration = config[app_context if app_context else 'development']
     app.config.from_object(configuration)
 
-    app.config['CHAOS_MIDDLEWARE_APPLICATION_NAME'] = 'MS1'
-    app.config['CHAOS_MIDDLEWARE_APPLICATION_ENV'] = 'development'
+    # app.config['CHAOS_MIDDLEWARE_APPLICATION_NAME'] = 'MS1'
+    # app.config['CHAOS_MIDDLEWARE_APPLICATION_ENV'] = 'development'
 
 
     db.init_app(app)
+    migrate.init_app(app, db, version_table='alembic_version_catalogo')
     cache.init_app(app, config=cache_config)
 
     from app.resource import catalogo
