ed1e8a9bca848360204076c8a749fa3231a616a7
Joaquin Lepez <joacolepez@gmail.com>
2025-01-21 11:36:51 -0300

    Delete cache

--- app/services/producto_service.py
+++ app/services/producto_service.py
@@ -1,21 +1,16 @@
 from ..repository import ProductoRepository
 from app.models import Producto
-from app import cache
 
 repository = ProductoRepository()
 
 class ProductoService:
 
     def all(self) -> list[Producto]:
-        result = cache.get('productos')
-        if result is None:
-            result = repository.all()
-            cache.set('productos', result, timeout=15)
+        result = repository.all()
         return result
     
     def add(self, producto: Producto) -> Producto:
         producto = repository.add_product(producto)
-        cache.set(f'producto_{producto.id}', producto, timeout=15)
         return producto
     
     def find_activo(self, id: int) -> Producto:
